8eab4889b0eeb149cc64d4e0c9371c0e0e4919d6
HCH270 <hchawkins@gmail.com>
2021-05-31 17:25:08 -0500

    lesson commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { GameControlComponent } from './ComponentsAndDatabinding/game-control/ga
 import { OddComponent } from './ComponentsAndDatabinding/odd/odd.component';
 import { EvenComponent } from './ComponentsAndDatabinding/even/even.component';
 import { ComsAndDbComponent } from './ComponentsAndDatabinding/coms-and-db/coms-and-db.component';
+import { RlComponent } from './RoutingLesson/rl.component';
+import { UsersComponent } from './RoutingLesson/users/users.component';
+import { UserComponent } from './RoutingLesson/users/user/user.component';
+import { ServerComponent } from './RoutingLesson/servers/server/server.component';
+import { ServersComponent } from './RoutingLesson/servers/servers.component';
+import { ServersService } from './RoutingLesson/servers/servers.service';
+import { EditServerComponent } from './RoutingLesson/servers/edit-server/edit-server.component';
+import { HomeComponent } from './RoutingLesson/home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,15 +23,23 @@ import { ComsAndDbComponent } from './ComponentsAndDatabinding/coms-and-db/coms-
     GameControlComponent,
     OddComponent,
     EvenComponent,
-    ComsAndDbComponent
+    ComsAndDbComponent,
+    RlComponent,
+    UsersComponent,
+    UserComponent,
+    ServerComponent,
+    ServersComponent,
+    EditServerComponent,
+    HomeComponent,
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
 
 
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
